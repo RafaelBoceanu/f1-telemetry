471598afb7e0ee730e2d250d6d80b12b58570976
RafaelBoceanu <rafaelboceanu@gmail.com>
2026-05-13 00:07:25 +0100

    Frontend scaffold - folder structure and types implemented

---
+++ frontend/src/types/f1.ts
@@ -0,0 +1,50 @@
+export interface Race {
+    round: number;
+    name: string;
+    country: string;
+    circuit: string;
+    date: string;
+}
+
+export interface Driver {
+    number: string;
+    abbreviation: string;
+    full_name: string;
+    team: string;
+    team_colour: string;
+}
+
+export interface TelemetryPoint {
+    time: number;
+    speed: number | null;
+    throttle: number | null;
+    brake: boolean,
+    gear: number | null;
+    rpm: number | null;
+    drs: number | null;
+    lap: number;
+}
+
+export interface PositionPoint {
+    time: number;
+    x: number | null;
+    y: number | null;
+    lap: number;
+}
+
+export interface PitStop {
+    lap: number;
+    compound: string | null;
+}
+
+export interface CircuitPoint {
+    x: number;
+    y: number;
+}
+
+export interface DriverData {
+    telemetry: TelemetryPoint[];
+    positions: PositionPoint[];
+    pit_stops: PitStop[];
+    circuit: CircuitPoint[];
+}
