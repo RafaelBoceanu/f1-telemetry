fd2b84edb6b733b4d632808c7f325fe1f179593b
RafaelBoceanu <rafaelboceanu@gmail.com>
2026-05-13 23:17:07 +0100

    Fixed URL bug

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://f1-telemetry-sigma.vercel.app/",
+        "https://f1-telemetry-sigma.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
